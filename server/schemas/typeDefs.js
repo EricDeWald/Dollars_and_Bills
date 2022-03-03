964e64943aa9d4d1a81d24e1a444dfb78cba997c
Skruphold <hunterco999@gmail.com>
2022-03-03 12:20:27 -0600

    Fixing some Errors

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,14 +12,14 @@ const typeDefs = gql`
     type Budget {
         _id: ID
         name: String
-        amount: INT
+        amount: Int
         expenses: [Expense]!
     }
 
     type Expense {
         _id: ID
         name: String
-        amount: INT
+        amount: Int
         description: String
         userId: User
     }
@@ -34,12 +34,14 @@ const typeDefs = gql`
         user(username: String!): User
         budgets(username: String): [Budget]
         budget(budgetId: ID!): Budget
+        expenses: [Expense]
+        expense: (expenseId: ID!): Expense
     }
 
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
-        addBudget(name: String!, amount: INT): Budget
+        addBudget(name: String!, amount: Int): Budget
         addExpense(budgetId: ID!, name: String!, description: String!, userId: User): Expense
         removeBudget(budgetId: ID!): Budget
         removeExpense(expenseId: ID!): Expense
