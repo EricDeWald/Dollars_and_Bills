6825bcbe168acc49bcda3753f9e1dba36252d42e
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-03-04 13:43:55 -0600

    changed class to class name in login and sign up, added budget form

--- client/src/components/BudgetCard/index.js
+++ client/src/components/BudgetCard/index.js
@@ -1,37 +1,44 @@
 import { Card, ProgressBar, Stack, Button } from 'react-bootstrap';
 
-export default function BudgetCard({ name, amount, max, gray }) {
+export default function BudgetCard({ budgets }) {
+    console.log(budgets)
     const classNames = []
-    if (amount > max) {
+    if (budgets.amount > budgets.max) {
         classNames.push('bg-danger', 'bg-opacity-10')
-    } else if (gray) {
+    } else if (budgets.gray) {
         classNames.push('bg-light')
     }
 
+
     return (
-        <Card className={classNames.join(' ')}>
-            <Card.Body>
-                <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
-                    <div className='me-2'>{name}</div>
-                    <div className='d-flex align-items-baseline'>${amount}
-                        <span className='text-muted fs-6 ms-1'>
-                            / ${max}
-                        </span>
-                    </div>
-                </Card.Title>
-                <ProgressBar 
-                    className='rounded-pill' 
-                    variant={progressBarColor(amount, max)}
-                    min={0}
-                    max={max}
-                    now={amount}
-                />
-                <Stack direction='horizontal' gap='2' className='mt-4'>
-                    <Button variant='outline-primary' className='ms-auto'>Add Expense</Button>
-                    <Button variant='outline-secondary'>View Expenses</Button>
-                </Stack>
-            </Card.Body>
-        </Card>
+        <div>
+            {budgets &&
+                budgets.map((budget) => (
+                    <Card key={budget._id} className={classNames.join(' ')}>
+                        <Card.Body>
+                            <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
+                                <div className='me-2'>{budget.name}</div>
+                                <div className='d-flex align-items-baseline'>${budget.amount}
+                                    <span className='text-muted fs-6 ms-1'>
+                                        / ${budget.max}
+                                    </span>
+                                </div>
+                            </Card.Title>
+                            <ProgressBar
+                                className='rounded-pill'
+                                variant={progressBarColor(budget.amount, budget.max)}
+                                min={0}
+                                max={budget.max}
+                                now={budget.amount}
+                            />
+                            <Stack direction='horizontal' gap='2' className='mt-4'>
+                                <Button variant='outline-primary' className='ms-auto'>Add Expense</Button>
+                                <Button variant='outline-secondary'>View Expenses</Button>
+                            </Stack>
+                        </Card.Body>
+
+                    </Card>
+                ))} </div>
     )
 }
 
@@ -39,7 +46,7 @@ function progressBarColor(amount, max) {
     const budgetProgress = amount / max;
     if (budgetProgress < 0.4) {
         return 'primary'
-    } else if (budgetProgress <0.8) {
+    } else if (budgetProgress < 0.8) {
         return 'warning'
     } else {
         return 'danger'
