8f557011ef42c8f7161bf8b8b526c3254888f212
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-03-05 11:38:46 -0600

    changed budget form to a modal, tested and is working to add to database. Hard coded in a now value of 25 to check progress bar. Moved add budget and add expense buttons into the budget form component for now.

--- client/src/components/BudgetCard/index.js
+++ client/src/components/BudgetCard/index.js
@@ -1,9 +1,13 @@
 import { Card, ProgressBar, Stack, Button } from 'react-bootstrap';
 
-export default function BudgetCard({ budgets }) {
-    console.log(budgets)
+
+export default function BudgetCard({ budgets }, now) {
+    console.log("budgets", budgets)
+    console.log("spread", { ...budgets })
+    // let spread = { ...budgets };
     const classNames = []
-    if (budgets.amount > budgets.max) {
+    ///currently budgets.amount will not work, needs to be looped over the spread of budgets
+    if (now > budgets.amount) {
         classNames.push('bg-danger', 'bg-opacity-10')
     } else if (budgets.gray) {
         classNames.push('bg-light')
@@ -11,34 +15,37 @@ export default function BudgetCard({ budgets }) {
 
 
     return (
-        <div>
-            {budgets &&
-                budgets.map((budget) => (
-                    <Card key={budget._id} className={classNames.join(' ')}>
-                        <Card.Body>
-                            <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
-                                <div className='me-2'>{budget.name}</div>
-                                <div className='d-flex align-items-baseline'>${budget.amount}
-                                    <span className='text-muted fs-6 ms-1'>
-                                        / ${budget.max}
-                                    </span>
-                                </div>
-                            </Card.Title>
-                            <ProgressBar
-                                className='rounded-pill'
-                                variant={progressBarColor(budget.amount, budget.max)}
-                                min={0}
-                                max={budget.max}
-                                now={budget.amount}
-                            />
-                            <Stack direction='horizontal' gap='2' className='mt-4'>
-                                <Button variant='outline-primary' className='ms-auto'>Add Expense</Button>
-                                <Button variant='outline-secondary'>View Expenses</Button>
-                            </Stack>
-                        </Card.Body>
+        <>
+            <div>
+                {budgets &&
+                    budgets.map((budget) => (
+                        <Card key={budget._id} className={classNames.join(' ')}>
+                            <Card.Body>
+                                <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
+                                    <div className='me-2'>{budget.name}</div>
+                                    <div className='d-flex align-items-baseline'>${25}
+                                        <span className='text-muted fs-6 ms-1'>
+                                            / ${budget.amount}
+                                        </span>
+                                    </div>
+                                </Card.Title>
+                                <ProgressBar
+                                    className='rounded-pill'
+                                    variant={progressBarColor(budget.amount, budget.max)}
+                                    min={0}
+                                    max={budget.amount}
+                                    now={25}
+                                />
+                                <Stack direction='horizontal' gap='2' className='mt-4'>
+                                    <Button variant='outline-primary' className='ms-auto'>Add Expense</Button>
+                                    <Button variant='outline-secondary'>View Expenses</Button>
+                                </Stack>
+                            </Card.Body>
 
-                    </Card>
-                ))} </div>
+                        </Card>
+                    ))} </div>
+
+        </>
     )
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,45 +1,38 @@
 import React from "react";
-import { Stack, Button } from 'react-bootstrap'
+import { Stack } from 'react-bootstrap'
 import Container from 'react-bootstrap/Container'
 import BudgetCard from '../components/BudgetCard';
-import { Link } from "react-router-dom";
-import BudgetForm from "../components/BudgetForm";
-import { QUERY_BUDGETS, QUERY_USER } from '../utils/queries';
+import { QUERY_USER } from '../utils/queries';
 import { useQuery } from "@apollo/client";
 import Auth1 from '../utils/auth';
+import BudgetForm from '../components/BudgetForm'
 
 const Home = () => {
     const userName = Auth1.getUsername();
-    console.log(userName)
+    console.log("localstorage", userName)
     const { loading, data } = useQuery(QUERY_USER, { variables: { username: userName } });
     let user;
-    console.log(data)
+    console.log("query", data)
 
     if (data) {
         user = data.user;
         console.log(user)
     }
 
-    // const { loading, data } = useQuery(QUERY_USER);
-    // console.log(data)
     const budgets = user?.budgets || [];
     return (
         <Container className='my-4'>
-            {/* <Link to="/login">← Go to Login</Link> */}
             <Stack direction='horizontal' gap='3' className='mb-4'>
                 <h1 className="me-auto">Budgets</h1>
-                <Button variant='primary'>Add Budget</Button>
-                <Button variant='outline-primary'>Add Expense</Button>
+                <BudgetForm></BudgetForm>
             </Stack>
             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '1rem', alignItems: 'flex-start' }}>
                 {loading ? (
                     <div>Loading...</div>
                 ) : (
-                    <BudgetCard budgets={budgets} />
+                    <BudgetCard budgets={budgets} now={25} />
                 )}
             </div>
-
-            <BudgetForm />
         </Container>
     )
 }
